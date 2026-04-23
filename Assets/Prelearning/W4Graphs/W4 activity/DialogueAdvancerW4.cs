2f68893c09b6d837021b68f9d3b8e9315b7fcadd
JingyiBi <17740878256@163.com>
2026-04-22 22:58:50 -0700

    inclass activity finished

--- Assets/Prelearning/W4Graphs/W4 activity/DialogueAdvancerW4.cs
+++ Assets/Prelearning/W4Graphs/W4 activity/DialogueAdvancerW4.cs
@@ -7,6 +7,7 @@ namespace W4Activity
     {
         // Singleton stuff- allows this class to be accessed from anywhere in the codebase without a reference (like a member variable)
         // do not edit this line
+        public DialogueNodeW4 startNode;
         public static DialogueAdvancerW4 _Instance {get; private set;}
 
         // More Singleton stuff
@@ -19,7 +20,10 @@ namespace W4Activity
 
             _Instance = this;
         }
-
+        private void Start()
+        {
+            EventBus.Trigger(VsEventNames.NewDialogueEvent, startNode);
+        }
         // Button hooks up to this method
         // do not edit this method
         public void ChooseDialogue (DialogueNodeW4 nextLine)
